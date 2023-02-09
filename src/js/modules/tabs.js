9a6e1d39c70a5d3e85315c4569c187b7fc5127ba
pozdnyakoks <pozdnyakoks@mail.ru>
2023-02-09 17:35:32 -0300

    add tabs

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,40 @@
+const tabsFunction = (headerSelector, tabsSelector, contentsSelector, activeClass) => {
+  const header = document.querySelector(headerSelector);
+  const tabs = document.querySelectorAll(tabsSelector);
+  const contents = document.querySelectorAll(contentsSelector);
+
+  const hideTabContent = () => {
+    contents.forEach(content => {
+      content.style.display = 'none';
+    });
+
+    tabs.forEach(tab => {
+      tab.classList.remove(activeClass)
+    })
+  }
+
+  const showTabContent = (i = 0) => {
+    contents[i].style.display = 'block';
+    tabs[i].classList.add(activeClass)
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  header.addEventListener('click', (ev) => {
+    const target = ev.target;
+    const selector = tabsSelector.replace(/\./, '')
+    if (target &&
+      (target.classList.contains(selector) ||
+        target.parentNode.classList.contains(selector))) {
+      tabs.forEach((tab, index) => {
+        if (target == tab || target.parentNode == tab) {
+          hideTabContent();
+          showTabContent(index)
+        }
+      })
+    }
+  })
+};
+
+export default tabsFunction;
